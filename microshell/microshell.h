d60e9132f5bba0bdeece0bee9fbdaed638765e02
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-10-14 16:25:41 +0200

    Fix commands list

--- microshell/microshell.h
+++ microshell/microshell.h
@@ -1,4 +1,4 @@
-#ifnset MICROSHELL_H
+#ifndef MICROSHELL_H
 # define MICROSHELL_H
 
 # define ERR_MSG_CD_ARGS "bad argumments"
@@ -6,5 +6,24 @@
 # define ERR_MSG_SYSCALL "fatal"
 # define ERR_MSG_EXECVE  "cannot execute"
 # include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+
+# define COMMAND	0
+# define PIPE		1
+# define SEMICOLON	2
+
+typedef struct		s_command
+{
+	unsigned char	type;
+	char			**arguments;
+}					t_command;
+
+typedef struct		s_list
+{
+	t_command		*command;
+	struct s_list	*next;
+}					t_list;
 
 #endif
