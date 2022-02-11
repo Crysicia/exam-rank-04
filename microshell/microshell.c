18f99d6471adbd2fb00c6f988de2207dfadac48d
Crysicia <7459685+Crysicia@users.noreply.github.com>
2022-02-11 17:16:44 +0100

    Exam done

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -1,27 +1,17 @@
-#ifdef TEST_SH
-# define TEST		1
-#else
-# define TEST		0
-#endif
-
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <stdbool.h>
 
-#include <stdio.h>
-
-void ft_putstr_err(char *str)
-{
-	size_t index = 0;
+void ft_putstr_err(char *str) {
+	int index = 0;
 
 	while (str[index])
 		index++;
 	write(STDERR_FILENO, str, index);
 }
 
-int ft_execute(char *argv[], int index, int stdin, char *envp[])
-{
+int ft_execute(char *argv[], int index, int stdin, char *envp[]) {
 	argv[index] = NULL;
 	close(stdin);
 	execve(argv[0], argv, envp);
@@ -31,19 +21,16 @@ int ft_execute(char *argv[], int index, int stdin, char *envp[])
 	return 1;
 }
 
-void close_pipe(int fds[2])
-{
+void close_pipe(int fds[2]) {
 	close(fds[0]);
 	close(fds[1]);
 }
 
-void cd_builtin(int args_length, char *args[])
-{
+void cd_builtin(int args_length, char *args[]) {
 	if (args_length != 2) {
 		ft_putstr_err("error: cd: bad arguments\n");
 		return ;
 	}
-
 	if (chdir(args[1])) {
 		ft_putstr_err("error: cd: cannot change directory to ");
 		ft_putstr_err(args[1]);
@@ -51,27 +38,20 @@ void cd_builtin(int args_length, char *args[])
 	}
 }
 
-int cmd_length(char *argv[])
-{
+int cmd_length(char *argv[]) {
 	int i = 0;
-	while (argv[i])
-	{
-		if (!strcmp(argv[i], ";") || !strcmp(argv[i], "|"))
-			return i;
+	while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
 		i++;
-	}
 	return i;
 }
 
-bool is_last_cmd(char *argv[], int index)
-{
+bool is_last_cmd(char *argv[], int index) {
 	if (!argv[index] || !strcmp(argv[index], ";"))
 		return true;
 	return false;
 }
 
-int main(int argc, char *argv[], char *envp[])
-{
+int main(int argc, char *argv[], char *envp[]) {
 	(void)argc;
 	int index = 0;
 	int current_pid = 0;
@@ -114,7 +94,5 @@ int main(int argc, char *argv[], char *envp[])
 		}
 	}
 	close(stdin);
-	if (TEST)		// not needed in exam, but necessary if you want to use this tester:
-		while (1);	// 
 	return 0;
 }
